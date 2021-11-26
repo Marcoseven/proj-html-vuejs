c0da608c4f7a9cb346023d264559e7f39cc4c01e
Marcoseven <marco.caliciotti3@gmail.com>
2021-11-26 15:49:52 +0100

    Aggiunte modifiche file SiteMain.vue
    Aggiunta importazione del file Project.vue.

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,18 +2,21 @@
 	<div id="SiteMain">
 		<Jumbotron />
 		<Services />
+		<Project />
 	</div>
 </template>
 
 <script>
 import Jumbotron from "./Jumbotron";
 import Services from "./Services";
+import Project from "./Project";
 
 export default {
 	name: "App",
 	components: {
 		Jumbotron,
 		Services,
+		Project,
 	},
 };
 </script>
